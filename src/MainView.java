a5cc7b8949cad1aba83fd01c4e701e1b6f8a1acb
Adlan Ramly <adlanarvyanda@gmail.com>
2016-11-26 21:40:52 -0500

    Added game mechanism pseudocode -Adlan

--- src/MainView.java
+++ src/MainView.java
@@ -8,6 +8,45 @@ import javax.swing.JPanel;
  * 
  * @author jrimland
  */
+
+//GAME MECHANISM PSEUDOCODE by Adlan
+
+//private num Scenes { //list all of the states here
+//sc1; 
+//};
+//
+//private Scenes myScene;
+//
+//void Start() {
+//	myScene = Scenes.sc1;
+//}
+//
+//void Update() {
+//Print (myScene);
+//
+//if (myScene == Scenes.sc1) {scene_sc1();}
+//}
+//
+//void sc1() {
+//Text.text = "//prompt here"
+//Button choice1 = "A";
+//Button choice2 = "B";
+//
+//if (choice1) {
+//myScene = Scenes.nextSceneA;
+//}
+//
+//if(choice2){
+//myScene = Scenes.nextSceneB;
+//}
+//
+//void nextSceneA(){
+//}
+//
+//void nextSceneB(){
+//}
+//}
+
 public class MainView extends JPanel{
     MainView()
     {
